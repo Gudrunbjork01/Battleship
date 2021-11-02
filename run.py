30c311b8ea43b24e5fffa8f8e101a99ba3dd6f7c
Gudrunbjork01 <annagudrun01@gmail.com>
2021-11-02 20:30:49 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import
+from word import word  # importing my word
+
+
+def get_valid_word(words):
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(word)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    lives = 7
