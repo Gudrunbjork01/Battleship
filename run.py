7aa4e12b000b2995ff57aa34071576839f6eeb91
Gudrunbjork01 <annagudrun01@gmail.com>
2021-11-03 23:35:47 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,18 +1,63 @@
-import
-from word import word  # importing my word
+inputWord = str.upper(input("Codeinstitute"))
+guessedLetter = ""
+wordLength = len(inputWord)
+wordList = [""] * wordLength
+usedLetters = [""]
+
+letterNumber = 0
+allowedGuesses = 10
+rightGuesses = 0
+winGuesses = 0
+
+print("here is some info")  # game rules
+print("Are you ready")
+print("the word is " + str(wordLength) + " 13 letters.")
+print("you have 10 tries ")
+print("good luck")
 
 
-def get_valid_word(words):
-    while '-' in word or ' ' in word:
-        word = random.choice(words)
+while(guessedLetter != "END") and (allowedGuesses != 0):
+    print(wordList)
+    guessedLetter = str.upper(input("Guess a letter: "))
 
-    return word.upper()
+    while letterNumber < wordLength:
+        if guessedLetter in usedLetters:
+            print("you have already guessed this letter.")
+            allowedGuesses += 1
+            break
+
+        elif inputWord[letterNumber] == guessedLetter:
+            wordList[letterNumber] = guessedLetter
+            rightGuesses = 1
+            winGuesses += 1
+
+        letterNumber += 1
+
+        allowedGuesses -= 1-rightGuesses
+        rightGuesses = 0
+        letterNumber = 0
+
+        usedLetters.append(guessedLetter)
+
+        if winGuesses == len(inputWord):
+            print(wordList)
+            print("Woho, you won " + inputWord)
+            break
+        elif allowedGuesses == 0:
+            print("game over the right word is" + inputWord)
+            break
+        elif guessedLetter == "END":
+            break
 
 
-def hangman():
-    word = get_valid_word(word)
-    word_letters = set(word)  # letters in the word
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
 
-    lives = 7
+ def main():  # code from tutorial on newlines
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+
+if __name__ == "__main__":
+    main()
